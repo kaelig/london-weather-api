3d1c2929cef8992d3ff198bd3009b7acadce2811
Kaelig <kaelig@deloumeau.fr>
2014-10-18 17:36:08 +0100

    Create londonweather.js

---
+++ londonweather.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/**
+ * London Weather library
+ *
+ * Useable methods are those documented with an @api in JSDoc
+ * See README.md for more explanations on how to use this stuff.
+ */
+function londonWeather () {
+  return {
+    whatstheweatherlike: function () {
+      return "Shitty";
+    }
+  }
+}
+
+module.exports = londonWeather;
